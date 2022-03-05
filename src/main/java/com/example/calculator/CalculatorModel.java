e3c60a9bb102b68e2aa394eec8df0f3d7736f276
MozamilS <smuzamel@gmail.com>
2022-03-06 01:05:49 +0430

    Initial Project Structure

---
+++ src/main/java/com/example/calculator/CalculatorModel.java
@@ -0,0 +1,15 @@
+package com.example.calculator;
+
+public class CalculatorModel {
+    public float calculation (float input1, float input2, String operators) {
+        switch (operators) {
+            case "+":
+                return input1 + input2;
+            case "-":
+                return input1 - input2;
+            case "/":
+                return input1 / input2;
+            case "*":
+                return input1 * input2;
+        }
+}
